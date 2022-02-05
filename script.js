2de2e85a8008f4efa3d56c6af04ef1b0bc2975a3
Jakub Szczepanowski <jakub.szczepanowski.rad@gmail.com>
2022-02-05 13:40:54 +0100

    Change timer

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function startTime() {
 
     document.getElementById('clock').innerHTML =
     h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+    setTimeout(startTime, 500);
 }
 function checkTime(i) {
     if (i < 10) {i = "0" + i};
@@ -16,20 +16,3 @@ function checkTime(i) {
 }
 
 startTime();
-var keyboardEvent = document.createEvent('KeyboardEvent');
-var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent';
-
-keyboardEvent[initMethod](
-  'keydown', // event type: keydown, keyup, keypress
-  true, // bubbles
-  true, // cancelable
-  window, // view: should be window
-  false, // ctrlKey
-  false, // altKey
-  false, // shiftKey
-  false, // metaKey
-  9, // keyCode: unsigned long - the virtual key code, else 0
-  0, // charCode: unsigned long - the Unicode character associated with the depressed key, else 0
-);
-document.dispatchEvent(keyboardEvent);
-document.dispatchEvent(keyboardEvent);
